7554199ae434490bdc30b4202ab1de3a769a2d98
varun <deviantbk201@gmail.com>
2023-10-31 17:38:53 +0530

    fixed  vercel .json agin

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App2.jsx";
+import App from "./App.jsx";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./ui/ErrorFallback";
 ReactDOM.createRoot(document.getElementById("root")).render(
